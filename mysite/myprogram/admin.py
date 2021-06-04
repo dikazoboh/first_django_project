28cb85ddb775664f4ab0dded91a53120fe6bd6d2
David I <35611032+dikazoboh@users.noreply.github.com>
2021-06-04 16:50:17 -0400

    this is the code for the django models assignment

--- mysite/myprogram/admin.py
+++ mysite/myprogram/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Student, School,Grade, Faculty, Department, CertificateType
 
 # Register your models here.
+
+admin.site.register(School)
+admin.site.register(Student)
+admin.site.register(CertificateType)
+admin.site.register(Faculty)
+admin.site.register(Department)
+admin.site.register(Grade)
--- mysite/myprogram/models.py
+++ mysite/myprogram/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=400)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Grade(models.Model):
+    type = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.type
+
+class CertificateType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Student(models.Model):
+    School = models.ForeignKey(School, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=40)
+    year_of_graduation=models.DateField(default=datetime.today)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    certificate_type = models.ForeignKey(CertificateType, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.fullname
+
+
+
+
+
+
+
